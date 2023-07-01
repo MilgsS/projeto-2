a24a9aa33a263b01b9facd1833a0dcc745c50616
MilgsS <127366107+MilgsS@users.noreply.github.com>
2023-07-01 18:25:46 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,25 +4,25 @@ const nomes = []
 const numeros = []
 
 
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+    addlinhas()
+})
+
 
 function addlinhas(){
     const tabela = document.querySelector('tbody');
-    tabela.innerHTML = linhas;
     const nome = document.getElementById('nome')
     const numero = document.getElementById('numero')
 
     nomes.push(nome.value)
     numeros.push(parseInt(numero.value))
-
     let linha = '<tr>'
     linha += `<td>${nome.value}</td>`
     linha += `<td>${numero.value}</td>`
     linha += '</tr>'
     linhas += linha
+    tabela.innerHTML = linhas;
     nome = ''
     numero = ''
 }
-form.addEventListener('submit', function(e){
-    e.preventDefault()
-    addlinhas()
-})
